721973a4128bdccb03009ac97123199d808f1a48
Dominik Panzarella <dominik.panzarella@gmail.com>
2023-06-22 12:09:48 +0200

    Initial commit

---
+++ src/SnakeGame.java
@@ -0,0 +1,21 @@
+/**
+ * ===============================================
+ *
+ *                Snake Game
+ *
+ * ===============================================
+ *
+ * @author Dominik Panzarella
+ * @version 1.0.1 2023-06-22
+ *
+ * ==============================================
+ *
+ */
+public class SnakeGame {
+
+    public static void main(String[] args) {
+
+        new GameFrame();
+
+    }
+}
